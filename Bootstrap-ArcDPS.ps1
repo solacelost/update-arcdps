b416d1a4cb65e3c8b730c4479921fd749e6c7fa0
James Harmison <jharmison@gmail.com>
2019-03-10 19:03:04 -0400

    Trial and error, the best medicine

--- Bootstrap-ArcDPS.ps1
+++ Bootstrap-ArcDPS.ps1
@@ -47,5 +47,5 @@ if ($myWindowsPrincipal.IsInRole($adminRole)) {
 }
 
 # This runs elevated as Administrator
-Set-ExecutionPolicy -ExecutionPolicy RemoteSigned -Confirm -Force -Scope LocalMachine
+Set-ExecutionPolicy -ExecutionPolicy RemoteSigned -Confirm:$False -Force -Scope LocalMachine
 pause
