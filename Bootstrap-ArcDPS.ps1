2edabfa1d340a5b8c35dc4ec6d97f069ddda9c60
James Harmison <jharmison@gmail.com>
2019-03-10 19:14:04 -0400

    Try, try again!

--- Bootstrap-ArcDPS.ps1
+++ Bootstrap-ArcDPS.ps1
@@ -1,46 +1,30 @@
-# Methodology (and chunks of code) taken directly from https://blogs.msdn.microsoft.com/virtual_pc_guy/2010/09/23/a-self-elevating-powershell-script/
-
 <# Run this with the following:
-powershell -NoProfile -Command "wget 'https://github.com/solacelost/update-arcdps/raw/master/Bootstrap-ArcDPS.ps1' -Outfile ($env:TEMP+'\bootstrap.ps1');Start-Process Powershell.exe -ArgumentList ('-ExecutionPolicy bypass -file '+$env:TEMP+'\bootstrap.ps1')"
+powershell -nop -c "iex(New-Object Net.WebClient).DownloadString('https://github.com/solacelost/update-arcdps/raw/master/Bootstrap-ArcDPS.ps1')"
 #>
 
-# Get the ID and security principal of the current user account
-$myWindowsID = [System.Security.Principal.WindowsIdentity]::GetCurrent()
-$myWindowsPrincipal = New-Object System.Security.Principal.WindowsPrincipal($myWindowsID)
+# First, download the Update-ArcDPS.ps1 script
+$ScriptPath = $env:APPDATA + '\Update-ArcDPS.ps1'
+Invoke-WebRequest "https://github.com/solacelost/update-arcdps/raw/master/Update-ArcDPS.ps1" -OutFile $ScriptPath
+# Then, set it to be locally executable
+Unblock-File $ScriptPath
 
-# Get the security principal for the Administrator role
-$adminRole=[System.Security.Principal.WindowsBuiltInRole]::Administrator
-# Check to see if we are currently running "as Administrator"
-if ($myWindowsPrincipal.IsInRole($adminRole)) {
-    # This runs elevated as Administrator
-    Set-ExecutionPolicy -ExecutionPolicy RemoteSigned -Confirm:$False -Force -Scope LocalMachine
-    pause
-} else {
-    $ScriptPath = $env:APPDATA + '\Update-ArcDPS.ps1'
-    # First, download the Update-ArcDPS.ps1 script
-    Invoke-WebRequest "https://github.com/solacelost/update-arcdps/raw/master/Update-ArcDPS.ps1" -OutFile $ScriptPath
-    # Then, set it to be locally executable
-    Unblock-File $ScriptPath
+# Drop a shortcut on the Desktop to setup the script
+$desktop = [system.environment]::GetFolderPath("Desktop")
+$ShortcutFile = "$desktop\Update-ArcDPS Setup.lnk"
+$WScriptShell = New-Object -ComObject WScript.Shell
+$Shortcut = $WScriptShell.CreateShortcut($ShortcutFile)
+$Shortcut.TargetPath = "%SystemRoot%\system32\WindowsPowerShell\v1.0\powershell.exe"
+$Shortcut.Arguments = "-File $ScriptPath -CreateShortcut"
+$Shortcut.Save()
 
-    # Drop a shortcut on the Desktop to setup the script
-    $desktop = [system.environment]::GetFolderPath("Desktop")
-    $ShortcutFile = "$desktop\Update-ArcDPS Setup.lnk"
-    $WScriptShell = New-Object -ComObject WScript.Shell
-    $Shortcut = $WScriptShell.CreateShortcut($ShortcutFile)
-    $Shortcut.TargetPath = "%SystemRoot%\system32\WindowsPowerShell\v1.0\powershell.exe"
-    $Shortcut.Arguments = "-File $ScriptPath -CreateShortcut"
-    $Shortcut.Save()
+# Now, spawn a new powershell window as admin to enable locally unsigned scripts
 
-    # Now, spawn a new powershell window to enable locally unsigned scripts
+# Create a new process object that starts PowerShell
+$newProcess = new-object System.Diagnostics.ProcessStartInfo "PowerShell"
+# Specify Set-ExecutionPolicy as the command to run
+$newProcess.Arguments = "-c 'Set-ExecutionPolicy -ExecutionPolicy RemoteSigned -Confirm:`$False -Force -Scope LocalMachine'"
+# Indicate that the process should be elevated
+$newProcess.Verb = "runas"
 
-    # Create a new process object that starts PowerShell
-    $newProcess = new-object System.Diagnostics.ProcessStartInfo "PowerShell"
-    # Specify the current script path and name as a parameter
-    $newProcess.Arguments = $myInvocation.MyCommand.Definition
-    # Indicate that the process should be elevated
-    $newProcess.Verb = "runas"
-
-    # Start the new process
-    [System.Diagnostics.Process]::Start($newProcess)
-    exit
-}
+# Start the new process
+[System.Diagnostics.Process]::Start($newProcess)
