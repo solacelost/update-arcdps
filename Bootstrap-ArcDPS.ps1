c5b6172ef6b29991a6ff4c5a56e4213cc08dd7d0
James Harmison <jharmison@gmail.com>
2019-03-10 18:27:37 -0400

    Testing bootstrap methodology

---
+++ Bootstrap-ArcDPS.ps1
@@ -0,0 +1,42 @@
+# Methodology (and chunks of code) taken directly from https://blogs.msdn.microsoft.com/virtual_pc_guy/2010/09/23/a-self-elevating-powershell-script/
+
+<# Run this with the following:
+powershell -NoProfile -Command "Invoke-WebRequest 'https://github.com/solacelost/update-arcdps/raw/master/Bootstrap-ArcDPS.ps1' -Outfile ($env:TEMP + Bootstrap-ArcDPS.ps1) ; Start-Process Powershell.exe -ArgumentList ($env:TEMP + Bootstrap-ArcDPS.ps1)"
+#>
+
+# Get the ID and security principal of the current user account
+$myWindowsID = [System.Security.Principal.WindowsIdentity]::GetCurrent()
+$myWindowsPrincipal = New-Object System.Security.Principal.WindowsPrincipal($myWindowsID)
+
+# Get the security principal for the Administrator role
+$adminRole=[System.Security.Principal.WindowsBuiltInRole]::Administrator
+# Check to see if we are currently running "as Administrator"
+if ($myWindowsPrincipal.IsInRole($adminRole)) {
+    # We are running "as Administrator" - so change the title and background color to indicate this
+    $Host.UI.RawUI.WindowTitle = $myInvocation.MyCommand.Definition + "(Elevated)"
+    $Host.UI.RawUI.BackgroundColor = "DarkBlue"
+    clear-host
+} else {
+    $ScriptPath = $env:APPDATA + '\Update-ArcDPS.ps1'
+    # First, download the Update-ArcDPS.ps1 script
+    Invoke-WebRequest "https://github.com/solacelost/update-arcdps/raw/master/Update-ArcDPS.ps1" -OutFile $ScriptPath
+    # Then, set it to be locally executable
+    Unblock-File $ScriptPath
+
+    # Now, spawn a new powershell window to enable locally unsigned scripts
+
+    # Create a new process object that starts PowerShell
+    $newProcess = new-object System.Diagnostics.ProcessStartInfo "PowerShell"
+    # Specify the current script path and name as a parameter
+    $newProcess.Arguments = $myInvocation.MyCommand.Definition
+    # Indicate that the process should be elevated
+    $newProcess.Verb = "runas"
+
+    # Start the new process
+    [System.Diagnostics.Process]::Start($newProcess)
+    # Exit from the current, unelevated, process
+    exit
+}
+
+# This runs elevated as Administrator
+Set-ExecutionPolicy -ExecutionPolicy RemoteSigned
