640bde4a35a5be059a1eba7da4c895dbff00aea1
James Harmison <jharmison@gmail.com>
2019-03-10 20:36:11 -0400

    This works - the GitHub CDN was delaying the push to me while testing, so it was probably working in one of the previous iterations. Whatever, this works.

--- Bootstrap-ArcDPS.ps1
+++ Bootstrap-ArcDPS.ps1
@@ -16,4 +16,5 @@ $Shortcut = $WScriptShell.CreateShortcut($ShortcutFile)
 $Shortcut.TargetPath = "%SystemRoot%\system32\WindowsPowerShell\v1.0\powershell.exe"
 $Shortcut.Arguments = "-ExecutionPolicy Bypass -File $ScriptPath -CreateShortcut"
 $Shortcut.Save()
+Write-Host "`nScript is bootstrapped! Run the Update-ArcDPS setup shortcut on your desktop."
 pause
