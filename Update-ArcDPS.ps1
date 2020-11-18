e9c28ce37bc1e124f56fd8e9ee19a2f118820de7
James Harmison <jharmison@gmail.com>
2020-11-18 18:36:47 -0500

    Added new update code that goes only off of md5sum

--- Update-ArcDPS.ps1
+++ Update-ArcDPS.ps1
@@ -580,9 +580,36 @@ if (Test-Path $SetupScript) {
 # Download ArcDPS
 $src = 'https://www.deltaconnected.com/arcdps/x64/'
 # To our GW2/bin64 directory
-$dst = $state.binpath
+$file = 'd3d9.dll'
+$sumfile = "$file.md5sum"
+# Save the expected md5sum as a string
+$arcdps_expected_sum = $utf8.GetString(
+    $(Invoke-WebRequest -UseBasicParsing -Uri "$src$sumfile").Content
+).Split(' ')[0]
 
-Download-Folder -src $src -dst $dst
+$md5 = New-Object -TypeName System.Security.Cryptography.MD5CryptoServiceProvider
+
+$ErrorActionPreference = "SilentlyContinue"
+$installed_sum = $(Get-FileHash $(Join-Path $state.binpath '/d3d9.dll')).Hash.ToLower()
+$ErrorActionPreference = "Continue"
+
+If ( $installed_sum -eq $arcdps_expected_sum ) {
+    Write-Host "Existing ArcDPS needs no update"
+} Else {
+    Write-Host "Downloading update to ArcDPS"
+    $arcdps = Invoke-WebRequest -UseBasicParsing -Uri "$src$file"
+
+    Write-Host "Validating update checksum"
+    $arcdps_sum = -Join $($md5.ComputeHash($arcdps.content) | ForEach {'{0:x2}' -f $_ })
+
+    If ( $arcdps_expected_sum -eq $arcdps_sum ) {
+        Write-Host "Expected checksum matches. Saving update to ArcDPS"
+        [System.IO.File]::WriteAllBytes($(Join-Path $state.binpath '/d3d9.dll'), $arcdps.content)
+    } Else {
+        Write-Error -Message "ArcDPS download did not match expected MD5 sum. Not saving file. Please try again later." `
+            -Category MetadataError -ErrorID MD5 -TargetObject $arcdps.content
+    }
+}
 
 Write-Host "Download of $src is complete."
 
